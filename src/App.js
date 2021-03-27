2417bcad325fa639ac09fef57050ef460fa3f964
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-03-26 20:03:50 -0500

    pages and routing with navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Roster from './pages/Roster'
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path='/search'>
+              <Search />
+            </Route>
+            <Route path='/roster'>
+              <Roster />
+            </Route>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
   );
 }
 
---
+++ src/pages/Roster.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Roster() {
+    return (
+        <div>
+            <h2>Roster</h2>
+        </div>
+    )
+}
+export default Roster
